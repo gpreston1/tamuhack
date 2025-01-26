08601cf233892e4cb151cb5d473566e46e52be93
namiyer <namiyer35@gmail.com>
2025-01-25 20:36:59 -0600

    Add files via upload

---
+++ question_bank.cs
@@ -0,0 +1,13 @@
+Dictionary<string, int> bank = new Dictionary<string, string>(10);
+
+bank.Add("Keeping old credit cards open", "Helps Credit");
+bank.Add("Paying off a loan", "Helps Credit");
+bank.Add("Reviewing your credit history for mistakes", "Helps Credit");
+bank.Add("Paying down high balances", "Helps Credit");
+bank.Add("Paying bills on time", "Helps Credit");
+
+bank.Add("Closing old credit cards", "Hurts Credit");
+bank.Add("Opening new credit cards frequently", "Hurts Credit");
+bank.Add("Maxing out your card", "Hurts Credit");
+bank.Add("Missing credit card payments", "Hurts Credit");
+bank.Add("Unpaid Medical Bills", "Hurts Credit");
